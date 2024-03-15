3b0bfe3a01665696e54074eaf3493c1ce28b56e5
Yash Thipsay <yashthpsay@gmail.com>
2024-03-15 14:43:58 +0530

    booking proxy deployment

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,10 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  const bookingProxy = await ethers.getContractFactory("BookingProxy");
+  const proxy = await upgrades.deployProxy(bookingProxy, [23]);
+  await proxy.deployed();
+
+  console.log(proxy.address);
+}
+main();
